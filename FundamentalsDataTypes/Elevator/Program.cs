c31f9710ee0dbe87bd1fab1d661a0c7a694df768
marin4o0 <marin4o0@hotmail.com>
2017-01-19 23:08:20 +0200

    New Fundamentals HomeWork Added!

---
+++ FundamentalsDataTypes/Elevator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Elevator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            var p = int.Parse(Console.ReadLine());
+            double divide = (n / p);
+            double remainder = p - (n % p);
+            Console.WriteLine(divide + remainder);
+        }
+    }
+}
---
+++ FundamentalsDataTypes/TriplesOfLetters/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TriplesOfLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var n = int.Parse(Console.ReadLine());
+            for (int num = 0; num < n; num++)
+                for (int num1 = 0; num1 < n; num1++)
+                    for (int num2 = 0; num2 < n; num2++)
+                    {
+                        char letter = (char)('a' + num);
+                        char letter1 = (char)('a' + num1);
+                        char letter2 = (char)('a' + num2);
+                        Console.WriteLine("{0}{1}{2}", letter, letter1, letter2);
+                    }
+        }
+    }
+}
