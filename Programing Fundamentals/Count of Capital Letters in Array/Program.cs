ca375a1e04a278e689021a8170f717d49d1d74c2
marin4o0 <marin4o0@hotmail.com>
2017-02-12 22:47:54 +0200

    New

---
+++ Programing Fundamentals/Count of Capital Letters in Array/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+public class Count_of_Capital_Letters_in_Array
+{
+    public static void Main()
+    {
+        var array = Console.ReadLine().ToString().ToArray();
+
+        var count = 0;
+
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (Char.IsUpper(array[i]))
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
