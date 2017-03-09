0af5bbb2c4f75bc8d13e8c83295d279755527e02
marin4o0 <marin4o0@hotmail.com>
2017-03-10 00:22:05 +0200

    some

---
+++ Programing Fundamentals/Dictionaries - Exercises/Dict-Ref/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Dict_Ref
+{
+    public class Dictionaries
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine();
+
+            var nameValue = new Dictionary<string, int>();
+
+            while (input != "end")
+            {
+                var splited = input.Split(new[] { '=' }, StringSplitOptions.RemoveEmptyEntries);
+
+                var name = splited[0];
+                var value = int.Parse(splited[1]);
+
+                nameValue[name] = value;
+
+                input = Console.ReadLine();
+            }
+            foreach (var item in nameValue)
+            {
+                Console.WriteLine("{0}=== {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
---
+++ Programing Fundamentals/Dictionaries - Exercises/Letter Repetition/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Letter_Repetition
+{
+    public class Dictionaries
+    {
+        public static void Main()
+        {
+            var input = Console.ReadLine().ToLower().ToCharArray();
+
+            var chars = new Dictionary<char, int>();
+
+            foreach (var character in input)
+            {
+                if (!chars.ContainsKey(character))
+                {
+                    chars[character] = 0;
+                }
+
+                chars[character]++;
+            }
+            foreach (var item in chars)
+            {
+                Console.WriteLine("{0} -> {1}", item.Key, item.Value);
+            }
+        }
+    }
+}
